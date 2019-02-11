e97d5f014823fbb3f3e395e6e0c86efb80ccb0fe
Tim Paine <t.paine154@gmail.com>
2019-02-11 13:58:03 -0500

    move to nose2

--- jupyterlab_celltests/tests.py
+++ jupyterlab_celltests/tests.py
@@ -1,27 +1,17 @@
 import nbformat
 import sys
-import nose
+import subprocess
 import os
 import os.path
 import tempfile
 
 base = '''import unittest
-import nose
-
 
 class TestExtension(unittest.TestCase):
 '''
 
 
-runner = '''
-
-if __name__ == '__main__':
-    argv = [{name}, '-v', '--with-coverage', '--cover-erase', '--cover-package=.']
-    nose.run(defaultTest={name}, argv=argv)
-'''
-
-
-def run(notebook_path):
+def run(notebook):
     nb = nbformat.read(notebook, 4)
     name = notebook.rsplit(os.path.sep, 1)[-1].replace('.ipynb', '') + '_test.py'
 
@@ -62,13 +52,13 @@ def run(notebook_path):
                 to_write.append(indent + 'pass')
 
             fp.writelines(to_write)
-        fp.write(runner.format(name=name))
+        fp.write('\n')
     return name
 
 if __name__ == '__main__':
     if len(sys.argv) != 2:
         raise Exception('Usage:python jupyterlab_celltests.tests <ipynb file>')
     notebook = sys.argv[1]
-    name = run(notebook)
-    argv = [name, '-v', '--with-coverage', '--cover-erase', '--cover-package=.']
-    nose.run(defaultTest=name, argv=argv)
+    name = run(notebook).replace('.py', '')
+    argv = ['nose2', name, '-v', '--with-coverage', '--coverage=' + name]
+    subprocess.call(argv)
