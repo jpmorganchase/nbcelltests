f64b1b0f458b3360822fb57a9607c1ee72bcb09c
Tim Paine <t.paine154@gmail.com>
2019-02-23 19:26:02 -0500

    fixes #12

--- jupyterlab_celltests/lint.py
+++ jupyterlab_celltests/lint.py
@@ -1,125 +1,133 @@
 import nbformat
 import sys
-import subprocess
 from .shared import extract_extrametadata
 
 
-def lint_lines_per_cell(fp, lines_per_cell, metadata):
+def lint_lines_per_cell(lines_per_cell, metadata):
     ret = ''
-    fail = False
+    passed = True
+
     if lines_per_cell:
         for i, lines_in_cell in enumerate(metadata.get('cell_lines', [])):
-            ret += "Checking lines in cell %d\n" % i
-            if lines_in_cell < lines_per_cell:
+            ret += "Checking lines in cell %d:" % i
+            if lines_in_cell <= lines_per_cell:
                 ret += "\tPASSED\n"
             else:
                 ret += "\tFAILED\n"
-                fail = True
-    return ret, fail
+                passed = False
+    return ret, passed
 
 
-def lint_cells_per_notebook(fp, cells_per_notebook, metadata):
+def lint_cells_per_notebook(cells_per_notebook, metadata):
     ret = ''
-    fail = False
+    passed = True
+
     if cells_per_notebook:
-        ret += 'Checking cells per notebook <= %d:\n' % cells_per_notebook
+        ret += 'Checking cells per notebook <= %d:' % cells_per_notebook
         if metadata.get('cell_count', -1) <= cells_per_notebook:
-            ret += "\tPASSED\n"
+            ret += "\tPASSED"
         else:
-            ret += "\tFAILED\n"
-            fail = True
-    return ret, fail
+            ret += "\tFAILED"
+            passed = False
+    return ret, passed
 
 
-def lint_function_definitions(fp, function_definitions, metadata):
+def lint_function_definitions(function_definitions, metadata):
     ret = ''
-    fail = False
+    passed = True
+
     if function_definitions:
-        ret += 'Checking functions per notebook <= %d:\n' % function_definitions
+        ret += 'Checking functions per notebook <= %d:' % function_definitions
         if metadata.get('functions', -1) <= function_definitions:
-            ret += "\tPASSED\n"
+            ret += "\tPASSED"
         else:
-            ret += "\tFAILED\n"
-            fail = True
-    return ret, fail
+            ret += "\tFAILED"
+            passed = False
+    return ret, passed
 
 
-def lint_class_definitions(fp, class_definitions, metadata):
+def lint_class_definitions(class_definitions, metadata):
     ret = ''
-    fail = False
+    passed = True
 
     if class_definitions:
-        ret += 'Checking classes per notebook <= %d:\n' % class_definitions
+        ret += 'Checking classes per notebook <= %d:' % class_definitions
         if metadata.get('classes', -1) <= class_definitions:
-            ret += "\tPASSED\n"
+            ret += "\tPASSED"
         else:
-            ret += "\tFAILED\n"
-            fail = True
-    return ret, fail
+            ret += "\tFAILED"
+            passed = False
+    return ret, passed
 
 
-def lint_cell_coverage(fp, cell_coverage, metadata):
+def lint_cell_coverage(cell_coverage, metadata):
     ret = ''
-    fail = False
+    passed = True
 
     if cell_coverage:
-        ret += "Checking cell test coverage >= %d:\n" % cell_coverage
+        ret += "Checking cell test coverage >= %d:" % cell_coverage
         if (metadata.get('test_count', 0)/metadata.get('cell_count', -1))*100 >= cell_coverage:
-            ret += "\tPASSED\n"
+            ret += "\tPASSED"
         else:
-            ret += "\tFAILED\n"
-            fail = True
-    return ret, fail
+            ret += "\tFAILED"
+            passed = False
+    return ret, passed
 
 
 def run(notebook):
     nb = nbformat.read(notebook, 4)
     extra_metadata = extract_extrametadata(nb)
+    ret = ''
+    passed = True
+
     if 'lines_per_cell' in extra_metadata:
         lines_per_cell = extra_metadata.get('lines_per_cell', -1)
-        lint_lines_per_cell(fp, lines_per_cell, extra_metadata)
+        lintret, lintfail = lint_lines_per_cell(lines_per_cell, extra_metadata)
+        ret += lintret
+        passed = passed and lintfail
 
     if 'cells_per_notebook' in extra_metadata:
         cells_per_notebook = extra_metadata.get('cells_per_notebook', -1)
-        lint_cells_per_notebook(fp, cells_per_notebook, extra_metadata)
+        lintret, lintfail = lint_cells_per_notebook(cells_per_notebook, extra_metadata)
+        ret += lintret + '\n'
+        passed = passed and lintfail
 
     if 'function_definitions' in extra_metadata:
         function_definitions = extra_metadata.get('function_definitions', -1)
-        lint_function_definitions(fp, function_definitions, extra_metadata)
+        lintret, lintfail = lint_function_definitions(function_definitions, extra_metadata)
+        ret += lintret + '\n'
+        passed = passed and lintfail
 
     if 'class_definitions' in extra_metadata:
         class_definitions = extra_metadata.get('class_definitions', -1)
-        lint_class_definitions(fp, class_definitions, extra_metadata)
+        lintret, lintfail = lint_class_definitions(class_definitions, extra_metadata)
+        ret += lintret + '\n'
+        passed = passed and lintfail
 
     if 'cell_coverage' in extra_metadata:
-        pass
-    return name
+        cell_coverage = extra_metadata.get('cell_coverage', 0)
+        lintret, lintfail = lint_cell_coverage(cell_coverage, extra_metadata)
+        ret += lintret + '\n'
+        passed = passed and lintfail
+    return ret, passed
 
 
 def runWithReturn(notebook):
-    name = run(notebook)
-    argv = ['py.test', name, '-v']
-    return subprocess.check_output(argv)
+    ret, fail = run(notebook)
+    return ret
 
 
 def runWithHTMLReturn(notebook):
-    name = run(notebook)
-    html = name.replace('.py', '.html')
-    argv = ['py.test', name, '-v', '--html=' + html, '--self-contained-html']
-    subprocess.call(argv)
-    with open(html, 'r') as fp:
-        return fp.read()
+    raise NotImplementedError()
 
 
 if __name__ == '__main__':
     if len(sys.argv) != 2:
-        raise Exception('Usage:python jupyterlab_celltests.tests <ipynb file>')
+        raise Exception('Usage:python jupyterlab_celltests.lint <ipynb file>')
     notebook = sys.argv[1]
-    name = run(notebook)
-    argv = ['py.test', name, '-v', '--html=' + name.replace('.py', '.html'), '--self-contained-html']
-    print(' '.join(argv))
-    subprocess.call(argv)
-
-    # doesnt refresh modules, dont use
-    # print('running from main')
-    # pytest.main([name, '-v', '--cov=' + name])
+    ret, passed = run(notebook)
+    if passed:
+        sys.exit(0)
+    else:
+        print(ret)
+        sys.exit(1)
---
+++ jupyterlab_celltests/shared.py
@@ -0,0 +1,51 @@
+# import pytest
+import re
+
+FUNCTION_REGEX = r'def (.*?):'
+CLASS_REGEX = r'class (.*?):'
+
+
+def extract_cellsources(notebook):
+    return [c['source'].split('\n') for c in notebook.cells]
+
+
+def extract_celltests(notebook):
+    return [c['metadata'].get('tests', []) for c in notebook.cells]
+
+
+def extract_extrametadata(notebook, override=None):
+    base = notebook.metadata.get('celltests', {})
+    override = override or {}
+    base['cell_count'] = 0
+    base['cell_tested'] = []
+    base['test_count'] = 0
+    base['cell_lines'] = []
+    base['lines'] = 0
+    base['functions'] = 0
+    base['classes'] = 0
+
+    foo_regex = re.compile(FUNCTION_REGEX)
+    class_regex = re.compile(CLASS_REGEX)
+
+    for c in notebook.cells:
+        base['cell_lines'].append(0)
+        base['cell_tested'].append(False)
+        base['cell_count'] += 1
+        for line in c['source'].split('\n'):
+            base['lines'] += 1
+            base['cell_lines'][-1] += 1
+            if re.search(foo_regex, line):
+                base['functions'] += 1
+            if re.search(class_regex, line):
+                base['classes'] += 1
+        for t in c['metadata'].get('tests', []):
+            if t.strip().startswith('%cell'):
+                base['test_count'] += 1
+                base['cell_tested'][-1] = True
+                break
+
+    # in case you want to override notebook settings
+    if override:
+        base.update(override)
+
+    return base
