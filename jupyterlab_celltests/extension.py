044db3c5cff5f6ff9a53ab456c687c4b19fd710f
Chris B <ceball@users.sf.net>
2019-10-22 20:29:01 +0100

    Fix for win paths.

--- jupyterlab_celltests/extension.py
+++ jupyterlab_celltests/extension.py
@@ -40,7 +40,7 @@ class RunCelltestsHandler(IPythonHandler):
     def post(self):
         body = json.loads(self.request.body)
         path = os.path.join(os.getcwd(), body.get('path'))
-        name = path.rsplit('/', 1)[1]
+        name = os.path.basename(path)
         ret = yield self._run(body, path, name)
         self.finish({'status': 0, 'test': ret})
 
