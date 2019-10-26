6f54516089e106dbd69310a9e2ebe817d3f98053
Chris B <ceball@users.sf.net>
2019-10-26 20:45:27 +0100

    Split up tests for clearer results.

--- tests/test_shared.py
+++ tests/test_shared.py
@@ -9,15 +9,32 @@ BASIC_NB = os.path.join(os.path.dirname(__file__), 'basic.ipynb')
 MORE_NB = os.path.join(os.path.dirname(__file__), 'more.ipynb')
 
 
-def test_extract_extrametadata_basic():
-    nb = nbformat.read(BASIC_NB, 4)
-    res = extract_extrametadata(nb)
-    assert res['functions'] == 2
-    assert res['classes'] == 2
+def _metadata(nb, what):
+    extra_metadata = extract_extrametadata(nbformat.read(nb, 4))
+    return extra_metadata[what]
 
 
-def test_extract_extrametadata_more():
-    nb = nbformat.read(MORE_NB, 4)
-    res = extract_extrametadata(nb)
-    assert res['functions'] == 1
-    assert res['classes'] == 1
+def test_extract_extrametadata_functions_basic():
+    assert _metadata(BASIC_NB, 'functions') == 2
+
+def test_extract_extrametadata_classes_basic():
+    assert _metadata(BASIC_NB, 'classes') == 2
+
+def test_extract_extrametadata_cell_count_basic():
+    assert _metadata(BASIC_NB, 'cell_count') == 5
+
+def test_extract_extrametadata_cell_lines_basic():
+    assert _metadata(BASIC_NB, 'cell_lines') == [1]*5
+
+
+def test_extract_extrametadata_functions_more():
+    assert _metadata(MORE_NB, 'functions') == 1
+
+def test_extract_extrametadata_classes_more():
+    assert _metadata(MORE_NB, 'classes') == 1
+
+def test_extract_extrametadata_cell_count_more():
+    assert _metadata(MORE_NB, 'cell_count') == 6
+
+def test_extract_extrametadata_cell_lines_more():
+    assert _metadata(MORE_NB, 'cell_lines') == [1]*4 + [2,3]
