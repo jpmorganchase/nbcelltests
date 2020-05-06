7916b1d5ce2b43841f70bd557964e44c1317428e
Chris B <ceball@users.sf.net>
2020-05-06 14:02:11 +0100

    Make fix

--- nbcelltests/tests/test_test.py
+++ nbcelltests/tests/test_test.py
@@ -25,6 +25,7 @@ TEST_FAIL = os.path.join(os.path.dirname(__file__), '_test_fail.ipynb')
 # kernel per notebook not per cell, so test like that.
 FRESH_KERNEL_PER_TEST = False
 
+
 def _check_fresh(t, force_fresh=False):
     if FRESH_KERNEL_PER_TEST or force_fresh:
         t.run_test("""
@@ -41,7 +42,7 @@ def _check_fresh(t, force_fresh=False):
 # cleaning up/documenting.
 
 
-def _import_from_path(pypath,f):
+def _import_from_path(pypath, f):
     # TODO consider py version compat
     import importlib.util
     spec = importlib.util.spec_from_file_location(pypath, f)
@@ -56,46 +57,44 @@ class _TestTest(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
         with tempfile.NamedTemporaryFile(suffix='.py') as f:
-            cls.generated_tests = _import_from_path("nbcelltests.tests.%s.%s"%(__name__,cls.__name__), run(cls.NBNAME, filename=f.name))
-        
+            cls.generated_tests = _import_from_path("nbcelltests.tests.%s.%s" % (__name__, cls.__name__), run(cls.NBNAME, filename=f.name))
+
 
 class TestTestCumulativeRun(_TestTest):
 
     NBNAME = CUMULATIVE_RUN
-    
+
     def test_state(self):
         # TODO: and split up, depending whether testing fresh kernel per test or not
-        
+
         t = self.generated_tests.TestNotebook()
         t.setup_class()
 
-        ######### check cell did not run
+        # check cell did not run
         _check_fresh(t)
         t.test_cell_0()
         _check_fresh(t, force_fresh=True)
 
-
-        ######### check cell ran
+        # check cell ran
         _check_fresh(t)
         t.test_cell_1()
         t.run_test("""
         assert x == 0, x
         """)
-        
-        ######### cumulative cells ran
+
+        # cumulative cells ran
         _check_fresh(t)
         t.test_cell_2()
         t.run_test("""
         assert x == 1, x
         """)
-        
-        ######### check test affects state
+
+        # check test affects state
         _check_fresh(t)
         t.test_cell_3()
         t.run_test("""
         assert x == 2, x
         """)
-
 
         t.teardown_class()
 
@@ -103,15 +102,14 @@ class TestTestCumulativeRun(_TestTest):
 class TestTestCellException(_TestTest):
 
     NBNAME = CELL_ERROR
-    
+
     def test_cell_exception(self):
         t = self.generated_tests.TestNotebook()
         t.setup_class()
 
-
-        ######### cell errors
+        # cell errors
         _check_fresh(t)
-        
+
         try:
             t.test_cell_0()
         except Exception as e:
@@ -120,24 +118,22 @@ class TestTestCellException(_TestTest):
         else:
             raise Exception("Cell should have errored out")
 
-        
         t.teardown_class()
 
 
 class TestTestTestException(_TestTest):
 
     NBNAME = TEST_ERROR
-    
+
     def test_exception_in_test(self):
         t = self.generated_tests.TestNotebook()
         t.setup_class()
 
-
-        ######### caught cell error
+        # caught cell error
         _check_fresh(t)
         t.test_cell_0()
-        
-        ######### test errors
+
+        # test errors
         _check_fresh(t)
         try:
             t.test_cell_1()
@@ -146,33 +142,26 @@ class TestTestTestException(_TestTest):
             assert e.args[0].endswith("My test is bad too")
         else:
             raise Exception("Test should have failed")
-                
 
-        
         t.teardown_class()
 
 
 class TestTestTestFail(_TestTest):
 
     NBNAME = TEST_FAIL
-    
+
     def test_expected_fail(self):
         t = self.generated_tests.TestNotebook()
         t.setup_class()
 
-
-        ######### caught cell error
+        # caught cell error
         _check_fresh(t)
         try:
-            t.test_cell_0()        
+            t.test_cell_0()
         except Exception as e:
             assert e.args[0].startswith("Cell execution caused an exception")
             assert e.args[0].endswith("x should have been -1 but was 1")
         else:
             raise Exception("Test should have failed")
-                
 
-        
         t.teardown_class()
-
-
