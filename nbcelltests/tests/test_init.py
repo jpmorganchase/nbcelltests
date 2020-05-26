92d7f8348242bf3197b9b1ab0a04a6fbc70a461f
Chris B <ceball@users.sf.net>
2020-05-26 19:04:56 +0100

    Fix another test of extension path.

--- nbcelltests/tests/test_init.py
+++ nbcelltests/tests/test_init.py
@@ -11,4 +11,4 @@ from nbcelltests import _jupyter_server_extension_paths
 
 class TestInit:
     def test__jupyter_server_extension_paths(self):
-        assert _jupyter_server_extension_paths() == [{"module": "nbcelltests.extension"}]
+        assert _jupyter_server_extension_paths() == [{"module": "nbcelltests"}]
