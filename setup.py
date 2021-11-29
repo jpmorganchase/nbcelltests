52c472173ef0fa5bb0d0b9fb1357181ba957fcfb
Tim Paine <t.paine154@gmail.com>
2021-11-28 21:58:54 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -76,10 +76,10 @@ setup(
     classifiers=[
         "Development Status :: 3 - Alpha",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
         "Framework :: Jupyter",
     ],
     cmdclass=cmdclass,
@@ -94,4 +94,5 @@ setup(
     },
     include_package_data=True,
     zip_safe=False,
+    python_requires=">=3.7",
 )
