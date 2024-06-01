c1650076d4aa1a10b594cc11e2a32f057aed7494
painebot <dev@paine.nyc>
2024-05-31 23:30:12 -0400

    Bump version: 0.2.3 → 0.3.0

--- nbcelltests/__init__.py
+++ nbcelltests/__init__.py
@@ -8,7 +8,7 @@
 from .lint import run as runLint  # noqa: F401
 from .test import run as runTest  # noqa: F401
 
-__version__ = "0.2.3"
+__version__ = "0.3.0"
 
 
 def _jupyter_server_extension_paths():
