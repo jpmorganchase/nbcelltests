c726e6bf74e09db56ebc62282cf61dee9d582c1e
painebot <dev@paine.nyc>
2024-06-04 12:51:53 -0400

    Bring in line with jupyter-fs, move back to typescript

--- js/style/index.js
+++ js/style/index.js
@@ -1,9 +1,9 @@
 /******************************************************************************
  *
- * Copyright (c) 2019, the jupyter-fs authors.
+ * Copyright (c) 2019, the nbcelltests authors.
  *
- * This file is part of the jupyter-fs library, distributed under the terms of
+ * This file is part of the nbcelltests library, distributed under the terms of
  * the Apache License 2.0.  The full license can be found in the LICENSE file.
  *
  */
-module.exports = "test-file-stub";
+import './index.css';
--- js/tests/assetsTransformer.js
+++ js/tests/assetsTransformer.js
@@ -1,8 +1,8 @@
 /******************************************************************************
  *
- * Copyright (c) 2019, the nbcelltests authors.
+ * Copyright (c) 2019, the jupyter-fs authors.
  *
- * This file is part of the nbcelltests library, distributed under the terms of
+ * This file is part of the jupyter-fs library, distributed under the terms of
  * the Apache License 2.0.  The full license can be found in the LICENSE file.
  *
  */
@@ -10,6 +10,6 @@ const path = require("path");
 
 module.exports = {
   process(src, filename, config, options) {
-    return `module.exports = ${JSON.stringify(path.basename(filename))};`;
+    return "module.exports = " + JSON.stringify(path.basename(filename)) + ";";
   },
 };
